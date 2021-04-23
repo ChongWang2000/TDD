8689cd624c7620cbc0da405206237b8bc246c4c3
buptwch2018211702 <1357527606@qq.com>
2021-04-23 14:20:04 +0800

    5.ppt Please enter the commit message for your changes. Lines starting

--- app/views.py
+++ app/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
 # Create your views here.
 
 
@@ -8,4 +7,6 @@ def index(request):
     return render(request,'index.html')
 
 def home_page(request):
-    return render(request,'home.html')
+    return render(request,'home.html',{
+        'new_item_text':request.POST.get('item_text',''),
+    })
