0b95f1a88395c8bdd8a2f5b2ed4ad38736a5f5d3
buptwch2018211702 <1357527606@qq.com>
2021-06-04 16:42:17 +0800

    new URL + viewl

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -35,50 +35,6 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    # def test_can_start_a_list_and_retrieve_it_later(self):
-    #     # Edith has heard about a cool new online to-do app. she goes to check out its homepage
-    #     # self.browser.get('http://localhost:8000/app/home')
-    #     self.browser.get(self.live_server_url + "/app/home")
-    #
-    #     # She notices the page title and header mention to-do lists
-    #     self.assertIn('To-Do', self.browser.title)
-    #     header_text = self.browser.find_element_by_tag_name('h1').text
-    #     self.assertIn('To-Do', header_text)
-    #
-    #     # she is invited to enter a to-do item straight away
-    #     inputbox = self.browser.find_element_by_id('id_new_item')
-    #     self.assertEqual(
-    #         inputbox.get_attribute('placeholder'),
-    #         'Enter a to-do item'
-    #     )
-    #
-    #     # she types "Buy peacock feathers" into a text box(Edith`s hobby is tying fly-fishing lures)
-    #     inputbox.send_keys('Buy peacock feathers')
-    #
-    #     # when she hits enter, the page updates, and now the page lists
-    #     # "1:Buy peacock 'feathers' as an item in a to-do list
-    #     inputbox.send_keys(Keys.ENTER)
-    #     self.wait_for_row_in_list_table('1: Buy peacock feathers')
-    #
-    #
-    #     # there is still a text box inviting her to add another item.
-    #     # she enters "use peacock feathers to make a fly"(Edith is very methodical)
-    #     inputbox = self.browser.find_element_by_id('id_new_item')
-    #     inputbox.send_keys('Use peacock feathers to make a fly')
-    #     inputbox.send_keys(Keys.ENTER)
-    #
-    #
-    #     # the page updates again, and now shows both items on her list
-    #     self.wait_for_row_in_list_table('1: Buy peacock feathers')
-    #     self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
-    #
-    #     # Edith wonders whether the site will remember her list. then she sees
-    #     # that the site has generated a unique URL for her -- there is some explanatory text to that effect
-    #     self.fail('Finish the test')
-    #     # she visits that URL - her to-do list is still there
-    #
-    #     # Satisfied, she goes back to sleep
-
 
     def test_can_start_a_list_for_one_user(self):
 
@@ -113,8 +69,8 @@ class NewVisitorTest(LiveServerTestCase):
 
 
         #the page updates again, and now show both items on her list
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         #satisfied, she goes back to sleep
 
