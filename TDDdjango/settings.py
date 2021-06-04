b95ecef3a382f71f8c7577b45e3556e46b972411
buptwch2018211702 <1357527606@qq.com>
2021-06-04 19:50:05 +0800

    set static_root

--- TDDdjango/settings.py
+++ TDDdjango/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app.apps.AppConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATIC_ROOT= os.path.abspath(  os.path.join(BASE_DIR, '../static'))
