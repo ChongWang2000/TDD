2d5362ad2b21b9de649b1d4badc124e75cfac526
buptwch2018211702 <1357527606@qq.com>
2021-06-04 16:01:11 +0800

    okkk

--- TDDdjango/urls.py
+++ TDDdjango/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # url(r'^app/', include('app.urls', 'app'), name='app'),
     url(r'^app/home',views.home_page , name='home'),
+    url(r'^lists/new$',views.new_list , name='new_list'),
+    url(r'^lists/the-only-list-in-the-world/$',views.view_list , name='view_list'),
 ]
--- app/urls.py
+++ app/urls.py
@@ -7,5 +7,6 @@ app_name='app'
 
 urlpatterns = [
     url(r'^index/',views.index,name='index'),
-    url(r'^home/',views.home_page,name='home')
+    url(r'^home/',views.home_page,name='home'),
+    url(r'^home/lists/the-only-list-in-the-world/$',views.home_page,name='home'),
 ]
